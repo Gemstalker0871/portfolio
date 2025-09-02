7f1e2c30fff216bd6351bb6d76312c31117d64fd
Sam <sambhavchugh2004@gmail.com>
2025-09-03 03:14:10 +0530

    next

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { object } from "framer-motion/client";
 import { BackgroundGradientAnimation } from "./gradientBg";
+import  {GlobeDemo}  from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -52,37 +53,41 @@ export const BentoGridItem = ({
 
       style={{
         background: 'rgb(4,7,29)',
-        backgroundColor: '"linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)"'
+        backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)'
       }}
     >
 
-      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img src={img} alt="img" className={cn(imgClassName, 'object-cover, object-center')}/>
+            <img src={img} alt="img" className={cn(imgClassName, 'object-cover object-center')}/>
           )}
         </div>
         <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
-          {spareImg && (<img src={spareImg} alt="spareImg" className={'object-cover, object-center w-full h-full'}/>)}
+          {spareImg && (<img src={spareImg} alt="spareImg" className={'object-cover object-center w-full h-full'}/>)}
         </div>
         {id===6 && (
           <BackgroundGradientAnimation>
             <div className="absolute z-50 flex items-center justify-center text-white font-bold">
 
             </div>
+
           </BackgroundGradientAnimation>
         )}
-      </div>
-      
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+        <div className={cn(
+          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+        )}>
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+            {description}
+          </div>
+          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
           {title}
         </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
         </div>
+
+        {id===2 && <GlobeDemo />}
       </div>
+      
     </div>
   );
 };
